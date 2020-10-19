ef41f1b2f62003aabe9e936f828cde699b810840
Tadeja Kersic <tadeja.kersic@gmail.com>
2020-10-19 15:40:25 +0200

    create core data entity

---
+++ Daily Journal/Daily Journal/Entry+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Entry+CoreDataClass.swift
+//  Daily Journal
+//
+//  Created by Tadeja Keršič on 19/10/2020.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Entry)
+public class Entry: NSManagedObject {
+
+}
