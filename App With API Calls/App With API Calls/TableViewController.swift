d9a82151ccc7a832304f612f1aa93860bb640c18
Tadeja Kersic <tadeja.kersic@gmail.com>
2020-10-20 12:26:18 +0200

    images in table view design

--- App With API Calls/App With API Calls/TableViewController.swift
+++ App With API Calls/App With API Calls/TableViewController.swift
@@ -20,13 +20,10 @@ class TableViewController: UITableViewController {
         if let url = URL(string: "https://picsum.photos/v2/list?page=2&limit=100") {
             var request = URLRequest(url: url)
             request.httpMethod = "GET"
-            print("get images")
             URLSession.shared.dataTask(with: request) {(data, response, error) in
                 if error != nil {
-                    print("There was an error")
                 } else if (data != nil) {
-                    print("success")
-                    print(String(data:data!, encoding: .utf8))
+                   // print(String(data:data!, encoding: .utf8))
                     if let imagesFromApi = try? JSONDecoder().decode([Image].self, from: data!) {
                         DispatchQueue.main.async {
                             self.images = imagesFromApi
@@ -45,10 +42,14 @@ class TableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell =  tableView.dequeueReusableCell(withIdentifier: "imageCell", for: indexPath)
         
-        let image = images[indexPath.row]
-        cell.textLabel?.text = image.author
+       // let image = images[indexPath.row]
+       // cell.textLabel?.text = image.author
         
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 330
     }
 
     @IBAction func reloadImages(_ sender: Any) {
