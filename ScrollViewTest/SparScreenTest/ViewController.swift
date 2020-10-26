848ec49b4ad6bd823177558d4dbef48285ae9486
Tadeja Kersic <tadeja.kersic@gmail.com>
2020-10-26 09:47:56 +0100

    add ScrollView project

---
+++ ScrollViewTest/SparScreenTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SparScreenTest
+//
+//  Created by Tadeja Keršič on 22/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
+
